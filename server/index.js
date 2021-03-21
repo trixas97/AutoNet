698f831cf2e3cbb59f28a68db65aa823cd3c03b0
trixas97 <trixasmixas@gmail.com>
2021-03-21 13:13:03 +0200

    server sockets for scan network

--- server/index.js
+++ server/index.js
@@ -5,14 +5,22 @@ const socket = require('socket.io');
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
+let io = socket(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+
+const deviceFinder = require('./routes/api/device_find.js')(io);
+
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-
-const deviceFinder = require('./routes/api/device_find.js');
+app.set('socketio', io);
 app.use('/api/devices', deviceFinder);
-
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
--- server/routes/api/device_find.js
+++ server/routes/api/device_find.js
@@ -1,86 +1,93 @@
+module.exports = function(io) {
 const express = require('express');
 const router = express.Router();
 const ipFinder = require('ip');
 const {spawn} = require('child_process');
-const request = require('request');
 const {PythonShell} = require('python-shell');
 const ping = require('ping');
-// const { resolve } = require('path');
 
 
-// Get Posts
-router.get('/', async (req,res) => {
-    let ipnet = (ipFinder.cidrSubnet(req.query.ip).networkAddress + '/' + ipFinder.cidrSubnet(req.query.ip).subnetMaskLength);
-    let ips = await ipsFinder(ipnet);
-    for(let i=0; i < 2; i++){
-        ips = await hostsFinder(ips);
-    }
-    res.send("ok");
-});
+io.on('connection', (socket) => {
+    console.log("Made socket connection", socket.id);
 
-
-
-function ipsFinder(ipnet) {
-    return new Promise(resolve => {
-        let shell = new PythonShell('server/python/network_ips.py', {mode: 'json', args: [ipnet]});
-        shell.on('message', function (message) {
-            console.log(message[0].ip);
-            resolve(message);
-        });
-    });
-}
-
-function deviceFinderFast(host) {
-    return new Promise(resolve => {
-        let shell = new PythonShell('server/python/network_devices_fast.py', {mode: 'json', args: [host]});
-        shell.on('message', function (message) {
-            resolve(message);
-        });
-    });
-}
-
-function deviceFinderSlow(host) {
-    let python = spawn('python', ['server/python/network_devices.py', host.ip]);
-    python.stdout.on('data', function(data) {
-        let datastr = data.toString();
-        let datajson = JSON.parse(datastr);
-        host.vendor = datajson.vendor;
-
-        if(host.vendor == "Cisco"){                                                   // Check vendor for Cisco devices
-            console.log(host);                                                        // Return Device from Slow script
+    // Find devices
+    router.get('/', async (req,res) => {
+        let ipnet = (ipFinder.cidrSubnet(req.query.ip).networkAddress + '/' + ipFinder.cidrSubnet(req.query.ip).subnetMaskLength);
+        let ips = await ipsFinder(ipnet);
+        for(let i=0; i < 2; i++){
+            ips = await hostsFinder(ips,req.query.id);
         }
+        res.send("ok");
     });
-}
 
 
-function hostsFinder(ips)  {
-    return new Promise(resolve => {
-        let count = 0;
-        let hosts = [];
-        let deadHosts = [];
-        ips.forEach(function(host){
-            ping.sys.probe(host.ip, async function(isAlive){
-                count++;
-                if(isAlive){
-                    let aliveHost = await deviceFinderFast(host.ip);
-                    if(aliveHost.vendor == null){
-                       deviceFinderSlow(aliveHost);                                    // Call Function => Return Device from Slow script
-                    }else{
-                        if(aliveHost.vendor == "Cisco Systems"){                       // Check vendor for Cisco devices
-                            console.log(aliveHost);                                    // Return Device from Fast script
-                        }
-                    }
-                    hosts.push(aliveHost);  
-                } else {
-                    deadHosts.push(host);
-                }
-                if(count == ips.length){
-                    console.log("size " + count);
-                    resolve(deadHosts);
-                }
+
+    function ipsFinder(ipnet) {
+        return new Promise(resolve => {
+            let shell = new PythonShell('server/python/network_ips.py', {mode: 'json', args: [ipnet]});
+            shell.on('message', function (message) {
+                console.log(message[0].ip);
+                resolve(message);
             });
         });
-    });
-}
+    }
 
-module.exports = router;
+    function deviceFinderFast(host) {
+        return new Promise(resolve => {
+            let shell = new PythonShell('server/python/network_devices_fast.py', {mode: 'json', args: [host]});
+            shell.on('message', function (message) {
+                resolve(message);
+            });
+        });
+    }
+
+    function deviceFinderSlow(host, socketid) {
+        let python = spawn('python', ['server/python/network_devices.py', host.ip]);
+        python.stdout.on('data', function(data) {
+            let datastr = data.toString();
+            let datajson = JSON.parse(datastr);
+            host.vendor = datajson.vendor;
+
+            if(host.vendor == "Cisco"){                                                   // Check vendor for Cisco devices
+                console.log(host);                                                        // Return Device from Slow script
+                io.to(socketid).emit('net-scan', host);
+            }
+        });
+    }
+
+
+    function hostsFinder(ips, socketid)  {
+        return new Promise(resolve => {
+            let count = 0;
+            let hosts = [];
+            let deadHosts = [];
+            ips.forEach(function(host){
+                ping.sys.probe(host.ip, async function(isAlive){
+                    count++;
+                    if(isAlive){
+                        let aliveHost = await deviceFinderFast(host.ip);
+                        if(aliveHost.vendor == null){
+                        deviceFinderSlow(aliveHost, socketid);                          // Call Function => Return Device from Slow script
+                        }else{
+                            if(aliveHost.vendor == "Cisco Systems"){                       // Check vendor for Cisco devices
+                                console.log(aliveHost);                                    // Return Device from Fast script
+                                io.to(socketid).emit('net-scan', aliveHost);
+                            }
+                        }
+                        hosts.push(aliveHost);  
+                    } else {
+                        deadHosts.push(host);
+                    }
+                    if(count == ips.length){
+                        console.log("size " + count);
+                        resolve(deadHosts);
+                    }
+                });
+            });
+        });
+    }
+
+});
+
+return router;
+}
