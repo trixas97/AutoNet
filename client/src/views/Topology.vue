df68c9e5870c0cd9a298f7906742b57d9c6c1b34
DESKTOP-S5EJIHK\Mike <trixasmixas@gmail.com>
2021-07-06 23:07:07 +0300

    [Client] Fix Topology view to get nodes from api

--- client/src/views/Topology.vue
+++ client/src/views/Topology.vue
@@ -11,7 +11,6 @@
         :key="'NodeLabel' + node"
         :ref="setItemRef"
         alt="">R{{node}}</label>
-    <input @click="newNodes" type="button">
   </div>
 </template>
 
@@ -19,7 +18,7 @@
 // @ is an alias to /src
 // let plainDraggable = require('plain-draggable').default;
 import NetNode from '@/objects/netnode.js'
-import Link from '@/objects/link.js'
+// import Link from '@/objects/link.js'
 const { getTopologyRequest } = require('../router/api');
 
 export default {
@@ -30,7 +29,8 @@ export default {
     data(){
         let nodes = [];
         let links = [];
-        let nodesSize = 3;
+        let nodesSize = 0;
+        let nodesData = [];
 
 
         return {
@@ -39,70 +39,37 @@ export default {
             labelRefs: [],
             nodes,
             links,
-            nodesSize
+            nodesSize,
+            nodesData
         }
     },
     methods:{
 
         setItemRef(el) {
-            if(el){
-                console.log('el');
-
-                el.src ? this.imgRefs.push(el) : this.labelRefs.push(el);
-                this.nodeRefs.push(el)
-                // console.log(this.nodeRefs[this.nodeRefs.length-1]);
-            }
-            console.log(this.imgRefs);
-            console.log(this.labelRefs);
-
+            if(el){ el.src ? this.imgRefs.push(el) : this.labelRefs.push(el); }
         },
-        elementsInit(){
-            document.onreadystatechange = () => {
-                if (document.readyState == "complete") {
-                    console.log("ok");
-                }
-            }
-        },
-        newLink(){
-            let start = 0;
-            let end = 1;
-            console.log(this.nodes[start].node);
-            console.log(this.nodes[end]);
-            this.links.push(new Link(this.nodes[start].node, this.nodes[end].node,"G0/1","G0/1"));
 
-            this.nodes[start].links.push(this.links[this.links.length - 1]);
-            this.nodes[end].links.push(this.links[this.links.length - 1]);
+        // newLink(){
+        //     let start = 0;
+        //     let end = 1;
+        //     console.log(this.nodes[start].node);
+        //     console.log(this.nodes[end]);
+        //     this.links.push(new Link(this.nodes[start].node, this.nodes[end].node,"G0/1","G0/1"));
 
-            this.nodes[start].dragLink(this.nodes[start].links);
-            this.nodes[end].dragLink(this.nodes[end].links);
-        },
-        newNodes(){
-            this.nodesSize++;
-            let nodeifs= [{
-                interface: ['G0/0', 'G0/1', 'G0/2'], 
-                status: ['down','down', 'down']
-            }];
-            nodeifs[1] = {
-                interface: ['G0/3', 'G0/4', 'G0/5'], 
-                status: ['up','up', 'up']
-            };
-            // console.log('els' + this.imgRefs[0]);
-            // for(let i=0; i < 10; i++){
-                this.nodes.push(new NetNode(this.imgRefs[0], this.labelRefs[0], [], nodeifs));
-                this.nodes[0].labelPosition();
-            // }
-        },
+        //     this.nodes[start].links.push(this.links[this.links.length - 1]);
+        //     this.nodes[end].links.push(this.links[this.links.length - 1]);
+
+        //     this.nodes[start].dragLink(this.nodes[start].links);
+        //     this.nodes[end].dragLink(this.nodes[end].links);
+        // },
         async getNodes(){
             const data = {
                 token: this.$store.state.User.token,
-                // nodes: ['60c66b2975aa7347c8f47b3f', '60c66b2975aa7347c8f47b3f', '60c66b2975aa7347c8f47b3f']
                 id: '60e20a22008d4e0dfc38934f'
             }
-
             const res = await getTopologyRequest(data);
-            // newNodes();
-            console.log(res);
-        }
+            return res.data.nodes;
+        },
     },
     beforeUpdate() {
         this.imgRefs = []
@@ -110,12 +77,24 @@ export default {
         this.nodeRefs = []
     },
     updated() {
-        // console.log(this.itemRefs)
+        let nodeifs= [{
+            interface: ['G0/0', 'G0/1', 'G0/2'], 
+            status: ['down','down', 'down']
+        }];
+        nodeifs[1] = {
+            interface: ['G0/3', 'G0/4', 'G0/5'], 
+            status: ['up','up', 'up']
+        };
+
+        for(let i=0; i < this.nodesSize; i++){
+            this.nodes.push(new NetNode(this.imgRefs[i], this.labelRefs[i], [], nodeifs));
+            this.nodes[i].labelPosition();
+        }
     },
-    mounted(){
-        this.elementsInit();
-        this.getNodes();
-    }      
+    async mounted(){
+        this.nodesData = await this.getNodes();
+        this.nodesSize = this.nodesData.length;       
+    }     
 }
 </script>
 <style lang="scss" scoped>
