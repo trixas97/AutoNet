c8606a19aa374d6c14cf6c75540826bf2c6d9b38
DESKTOP-S5EJIHK\Mike <trixasmixas@gmail.com>
2021-08-01 17:14:48 +0300

    Init Quasar (Vue Ui Library)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,64 +1,42 @@
 <template>
-  <div id="app" class="app">
-    <div v-bind:class="{'container': $route.name != 'Login'}">
-      <NavBar v-if="$route.name != 'Login'" />
-      <router-view class="view"/>
-    </div>
-  </div>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
+import { ref } from 'vue'
+
 export default {
-  name: 'App',
+  name: 'LayoutDefault',
+
   components: {
-    NavBar
+  },
+
+  setup () {
+    return {
+      leftDrawerOpen: ref(false)
+    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-@import "./style/variables";
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
-* {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    
-    height: 100%;
-    
-    .container {
-      height: 100%;
-      display: grid;
-      grid-gap: 10px;
-      grid-template-rows: 0.7fr 0.4fr 10fr;
-      grid-template-areas: 
-        "navbar"
-        "."
-        "view";
-      .navbar {
-        grid-area: navbar;
-        background-color: $primary;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-      }
-      .view {
-        grid-area: view;
-        // margin-top: 1.5em;
-        height: 100%;
-      }
-    }
-    .container > div {
-      text-align: center;
-      font-size: 25px;
-      height: 100%;
-      color: white;
-    }
-  }
-}
-</style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
 
 
-const app = createApp(App);
+const app = createApp(App).use(Quasar, quasarUserOptions);
 app.use(router);
 app.use(store);
 app.mount('#app');
