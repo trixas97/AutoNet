2d11eae99a0937fca7d9062b7ac1b83a2d1c4686
DESKTOP-S5EJIHK\Mike <trixasmixas@gmail.com>
2021-09-03 00:14:11 +0300

    [Client] Fix design table of interfaces -> Interfaces.vue

--- client/src/components/Device/Interfaces.vue
+++ client/src/components/Device/Interfaces.vue
@@ -1,141 +1,238 @@
 <template>
-    <div class="container">
-        <div class="head">
-            <h4 class="title">Interfaces</h4>
-            <div class="select"><q-select standout v-model="model" :options="options" label="Standout" /></div>           
-        </div>
-        <div class="data">
-            <q-table
-                class="table"
-                :rows="rows"
-                row-key="key"
-                hide-pagination
-                virtual-scroll
-                
-            />
-        </div>
+<div class="container">
+    <q-table
+      class="my-sticky-header-table"
+      title="Treats"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      flat
+      bordered
+      hide-pagination 
+      :pagination="pagination"
+    >
+    <template v-slot:top>
+        <label>Interfaces</label>
+
+        <q-space />
+
+
+
+        <q-select standout v-model="model" :options="options"  label-color="white" color="teal"  bg-color="blue"/>
+      </template>
+    </q-table>
     </div>
 </template>
 
 <script>
-
 import { ref } from 'vue'
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Option',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: false
+  },
+  { name: 'calories', align: 'left', label: 'Value', field: 'calories', sortable: false },
+//   { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+//   { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+//   { name: 'protein', label: 'Protein (g)', field: 'protein' },
+//   { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+//   { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+//   { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+]
 
+const rows = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0,
+    sodium: 87,
+    calcium: '14%',
+    iron: '1%'
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+    fat: 9.0,
+    carbs: 37,
+    protein: 4.3,
+    sodium: 129,
+    calcium: '8%',
+    iron: '1%'
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+    fat: 16.0,
+    carbs: 23,
+    protein: 6.0,
+    sodium: 337,
+    calcium: '6%',
+    iron: '7%'
+  },
+  {
+    name: 'Cupcake',
+    calories: 305,
+    fat: 3.7,
+    carbs: 67,
+    protein: 4.3,
+    sodium: 413,
+    calcium: '3%',
+    iron: '8%'
+  },
+  {
+    name: 'Gingerbread',
+    calories: 356,
+    fat: 16.0,
+    carbs: 49,
+    protein: 3.9,
+    sodium: 327,
+    calcium: '7%',
+    iron: '16%'
+  },
+  {
+    name: 'Jelly bean',
+    calories: 375,
+    fat: 0.0,
+    carbs: 94,
+    protein: 0.0,
+    sodium: 50,
+    calcium: '0%',
+    iron: '0%'
+  },
+  {
+    name: 'Lollipop',
+    calories: 392,
+    fat: 0.2,
+    carbs: 98,
+    protein: 0,
+    sodium: 38,
+    calcium: '0%',
+    iron: '2%'
+  },
+  {
+    name: 'Honeycomb',
+    calories: 408,
+    fat: 3.2,
+    carbs: 87,
+    protein: 6.5,
+    sodium: 562,
+    calcium: '0%',
+    iron: '45%'
+  },
+  {
+    name: 'Donut',
+    calories: 452,
+    fat: 25.0,
+    carbs: 51,
+    protein: 4.9,
+    sodium: 326,
+    calcium: '2%',
+    iron: '22%'
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: '12%',
+    iron: '6%'
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: '12%',
+    iron: '6%'
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: '12%',
+    iron: '6%'
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: '12%',
+    iron: '6%'
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: '12%',
+    iron: '6%'
+  }
+]
+      let options= [
+        'F0/0', 'F0/1', 'F0/2', 'F0/3', 'F0/4'
+      ]
 export default {
   setup () {
-    let rows = [
-        {
-            key: "key 1",
-            value: "value1"
-        },
-        {
-            key: "key 2",
-            value: "value2"
-        },
-                {
-            key: "key 1",
-            value: "value1"
-        },
-        {
-            key: "key 2",
-            value: "value2"
-        },
-                {
-            key: "key 1",
-            value: "value1"
-        },
-        {
-            key: "key 2",
-            value: "value2"
-        },
-                {
-            key: "key 1",
-            value: "value1"
-        },
-        {
-            key: "key 2",
-            value: "value2"
-        },
-                {
-            key: "key 1",
-            value: "value1"
-        }
-        // {
-        //     key: "key 3",
-        //     value: "value3"
-        // },
-    ]
     return {
-      model: ref(null),
-      options: [
-        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
-      ],
-      rows
+      columns,
+      rows,
+        pagination: ref({
+        rowsPerPage: 0
+      }),      
+      options,
+      model: ref(options[0]),
+
     }
   }
 }
-
 </script>
 
-<style lang="scss" scoped>
-    .container {
-        display: grid;
-        grid-template-rows: 0.25fr 1fr;
-        grid-template-areas: 
-        "head"
-        "data";
-        border-bottom: black;
+<style lang="sass">
+.my-sticky-header-table
+  height: 100%
+  width: 100%
 
-        .head {
-            grid-area: head;
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            grid-template-areas: 
-            "title select";
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th
+    background-color: teal
+    color: white
 
-            .title {
-                grid-area: title;
-                padding-top: 0.1em;
-                padding-left: 0.3em;
-                margin: auto 0px auto 0px;
-
-            }
-
-            .select {
-                grid-area: select;
-                margin: 0px;
-                margin: auto 1em auto auto;
-
-                .q-select {
-                    width: 15em;
-                    margin-top: auto;
-                    margin-bottom: auto;
-                }
-            }
-
-        }
+  label
+    font-size: 2.2em
+    font-weight: bold
+  thead tr th
+    font-weight: bold
+    font-family: "Roboto"
+    font-size: 1.2em
+    position: sticky
+    z-index: 1
+    box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2)
+  thead tr:first-child th
+    top: 0
 
 
-        .data {
-            grid-area: data;
-            background-color: chartreuse;
-            max-height: 100vh;
-
-            .table {
-                // min-height: 200px;
-                .q-table {
-                    background-color: blue;
-                }
-                max-height: 250px;
-                tr th {
-                    position: sticky;
-                    background-color: blue;
-                }
-                // .q-table th{
-                //      background-color: blue;
-                // }
-            }
-
-        }
-    }
+  span
+    color: white
+  &.q-table--loading thead tr:last-child th
+    // top: 48px
 </style>
