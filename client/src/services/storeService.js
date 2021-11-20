d143cc3531d4e7f957362cddcac83b6ac021ebc1
DESKTOP-S5EJIHK\Mike <trixasmixas@gmail.com>
2021-11-20 16:01:13 +0200

    [Client] Fix store service traffic bug

--- client/src/services/storeService.js
+++ client/src/services/storeService.js
@@ -1,16 +1,25 @@
 import store from '@/store';
 
 export const updateTraffic = (data) => {
-    let nodes = store.getters['UserData/getNodes'].data;
-    data.forEach(traffic => {
-        nodes.forEach(node => {
-            node.interfaces.forEach(inter => {
-                if(inter.ip_address.value.includes(traffic.host)){
-                    inter.traffic.value.push(traffic)
-                    store.dispatch('UserData/setNode', node)                      
-                }
-            })
+    data.map(async traffic => {
+        let node = await findNodeByIp(traffic.host);
+        node.interfaces.map(inter => {
+            if(inter.interface.value == traffic.interface){
+                inter.traffic.value.push(traffic)
+                store.dispatch('UserData/setNode', node)                      
+            }
         })
+    })
+}
 
-    });
+const findNodeByIp = (ip) => {
+    return new Promise(resolve => {
+        let nodes = store.getters['UserData/getNodes'].data;
+        for(let i=0; i < nodes.length; i++){
+            nodes[i].interfaces.map(inter => {
+                if(inter.ip_address.value.includes(ip+'/'))
+                    resolve(nodes[i])
+            })
+        }
+    })
 }
